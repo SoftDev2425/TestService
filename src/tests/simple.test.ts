f3c0331fb51858abb5a93b4e0c90beafc2254421
Owais Dashti <owaisdashti@outlook.com>
2024-10-17 12:13:47 +0200

    fixing test

--- src/tests/simple.test.ts
+++ src/tests/simple.test.ts
@@ -3,6 +3,6 @@ import { someFunc } from '../utils/simple';
 describe('Some test', () => {
   it('should return concatenated', () => {
     const result = someFunc('hello', 'world');
-    expect(result).toBe('hello:world2');
+    expect(result).toBe('hello:world');
   });
 });
